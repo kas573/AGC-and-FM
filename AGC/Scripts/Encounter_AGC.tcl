34452adeba83c23bc17e8d2455e1cc628465e8cd
kas573 <75730787+kas573@users.noreply.github.com>
2020-12-20 21:51:35 +0300

    Layout
    Script for layout generation

---
+++ AGC/Scripts/Encounter_AGC.tcl
@@ -0,0 +1,155 @@
+#######################################################
+#                                                     
+#  Encounter Command Logging File                     
+#  Created on Wed Dec  2 23:20:52 2020                
+#                                                     
+#######################################################
+
+#@(#)CDS: Encounter v14.28-s033_1 (64bit) 03/21/2016 13:34 (Linux 2.6.18-194.el5)
+#@(#)CDS: NanoRoute v14.28-s005 NR160313-1959/14_28-UB (database version 2.30, 267.6.1) {superthreading v1.25}
+#@(#)CDS: CeltIC v14.28-s006_1 (64bit) 03/08/2016 00:08:23 (Linux 2.6.18-194.el5)
+#@(#)CDS: AAE 14.28-s002 (64bit) 03/21/2016 (Linux 2.6.18-194.el5)
+#@(#)CDS: CTE 14.28-s007_1 (64bit) Mar  7 2016 23:11:05 (Linux 2.6.18-194.el5)
+#@(#)CDS: CPE v14.28-s006
+#@(#)CDS: IQRC/TQRC 14.2.2-s217 (64bit) Wed Apr 15 23:10:24 PDT 2015 (Linux 2.6.18-194.el5)
+
+set_global _enable_mmmc_by_default_flow      $CTE::mmmc_default
+suppressMessage ENCEXT-2799
+win
+#Loading source files and defining power and ground nets
+set init_gnd_net VSS
+set init_lef_file {/Cadence/Libs/X_FAB/XKIT/xt018/cadence/v5_0/techLEF/v5_0_2/xt018_xx43_MET4_METMID_METTHK.lef /Cadence/Libs/X_FAB/XKIT/xt018/diglibs/D_CELLS_HD/v4_0/LEF/v4_0_0/xt018_D_CELLS_HD.lef}
+set init_design_settop 0
+set init_verilog ../Outputs/TYP_CORNER/AGC_Top_synth.v
+set init_mmmc_file ../Source/MMMC_AGC_regs.tcl
+set init_io_file ../Source/AGC_pins
+set init_pwr_net VDD
+#Design initializing
+init_design
+#Specifying floorplan
+floorPlan -site core_hd -r 0.9795675756 0.699999 10.08 10.37 10.08 10.37
+#Global nets connection
+globalNetConnect VDD -type pgpin -pin vdd -inst * -module {}
+globalNetConnect VDD -type tiehi -pin vdd -inst * -module {}
+globalNetConnect VSS -type pgpin -pin gnd -inst * -module {}
+globalNetConnect VSS -type tielo -pin gnd -inst * -module {}
+#Power and ground rings creation
+set sprCreateIeRingNets {}
+set sprCreateIeRingLayers {}
+set sprCreateIeRingWidth 1.0
+set sprCreateIeRingSpacing 1.0
+set sprCreateIeRingOffset 1.0
+set sprCreateIeRingThreshold 1.0
+set sprCreateIeRingJogDistance 1.0
+addRing -skip_via_on_wire_shape Noshape -skip_via_on_pin Standardcell -stacked_via_top_layer METTPL -type core_rings -jog_distance 3.15 -threshold 3.15 -nets {VDD VSS} -follow core -stacked_via_bottom_layer MET1 -layer {bottom MET1 top MET1 right MET2 left MET2} -width 3 -spacing {bottom 0.23 top 0.23 right 0.28 left 0.28} -offset 3.15
+#Vertical power stripes creation
+set sprCreateIeStripeNets {}
+set sprCreateIeStripeLayers {}
+set sprCreateIeStripeWidth 10.0
+set sprCreateIeStripeSpacing 2.0
+set sprCreateIeStripeThreshold 1.0
+addStripe -skip_via_on_wire_shape Noshape -block_ring_top_layer_limit MET3 -max_same_layer_jog_length 6 -padcore_ring_bottom_layer_limit MET1 -set_to_set_distance 100 -skip_via_on_pin Standardcell -stacked_via_top_layer METTPL -padcore_ring_top_layer_limit MET3 -spacing 0.28 -merge_stripes_value 3.15 -layer MET2 -block_ring_bottom_layer_limit MET1 -width 3 -nets {VDD VSS} -stacked_via_bottom_layer MET1
+#Horizontal power stripes creation
+sroute -connect { blockPin padPin padRing corePin floatingStripe } -layerChangeRange { MET1 METTPL } -blockPinTarget { nearestTarget } -padPinPortConnect { allPort oneGeom } -padPinTarget { nearestTarget } -corePinTarget { firstAfterRowEnd } -floatingStripeTarget { blockring padring ring stripe ringpin blockpin followpin } -allowJogging 1 -crossoverViaLayerRange { MET1 METTPL } -nets { VDD VSS } -allowLayerChange 1 -blockPin useLef -targetViaLayerRange { MET1 METTPL }
+#PrePlace setup timing report
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -prePlace -idealClock -pathReports -drvReports -slackReports -numPaths 50 -prefix AGC_prePlace -outDir ../Reports/Layout/timingReports/prePlace
+setMultiCpuUsage -localCpu 8 -cpuPerRemoteHost 1 -remoteHost 0 -keepLicense true
+#Place standart cells
+setDistributeHost -local
+setPlaceMode -fp false
+placeDesign
+#PreCTS setup timing report
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -preCTS -idealClock -pathReports -drvReports -slackReports -numPaths 50 -prefix AGC_preCTS -outDir ../Reports/Layout/timingReports/preCTSsetup
+#PreCTS hold timing report
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -preCTS -hold -idealClock -pathReports -slackReports -numPaths 50 -prefix AGC_preCTS -outDir ../Reports/Layout/timingReports/preCTShold
+#PreCTS optimization
+setOptMode -fixCap true -fixTran true -fixFanoutLoad false
+optDesign -preCTS
+#PreCTS timing report after optimization
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -preCTS -idealClock -pathReports -drvReports -slackReports -numPaths 50 -prefix AGC_preCTS -outDir ../Reports/Layout/timingReports/preCTSsetup_optimized
+#Clock tree synthesis
+createClockTreeSpec -bufferList BUHDX12 -file Clock.ctstch
+setCTSMode -engine ck
+clockDesign -specFile Clock.ctstch -outDir clock_report -fixedInstBeforeCTS
+#PostCTS setup timing report
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -postCTS -pathReports -drvReports -slackReports -numPaths 50 -prefix AGC_postCTS -outDir ../Reports/Layout/timingReports/PostCTSsetup
+#PostCTS hold timing report
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -postCTS -hold -pathReports -slackReports -numPaths 50 -prefix AGC_postCTS -outDir ../Reports/Layout/timingReports/PostCTShold
+#PostCTS optimization
+setOptMode -fixCap true -fixTran true -fixFanoutLoad false
+optDesign -postCTS
+optDesign -postCTS -hold
+setOptMode -fixCap false -fixTran false -fixFanoutLoad false
+optDesign -postCTS -incr
+optDesign -postCTS -hold -incr
+#PostCTS setup timing report after optimization
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -postCTS -pathReports -drvReports -slackReports -numPaths 50 -prefix AGC_postCTS -outDir ../Reports/Layout/timingReports/PostCTSsetup_optimized
+#Rooting
+setNanoRouteMode -quiet -routeInsertAntennaDiode 1
+setNanoRouteMode -quiet -timingEngine {}
+setNanoRouteMode -quiet -routeWithSiPostRouteFix 0
+setNanoRouteMode -quiet -drouteStartIteration default
+setNanoRouteMode -quiet -routeTopRoutingLayer default
+setNanoRouteMode -quiet -routeBottomRoutingLayer default
+setNanoRouteMode -quiet -drouteEndIteration default
+setNanoRouteMode -quiet -routeWithTimingDriven false
+setNanoRouteMode -quiet -routeWithSiDriven false
+routeDesign -globalDetail
+setAnalysisMode -analysisType onChipVariation -skew true -clockPropagation sdcControl
+#PostRoute setup timing report
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -postRoute -pathReports -drvReports -slackReports -numPaths 50 -prefix AGC_postRoute -outDir ../Reports/Layout/timingReports/PostRoutesetup
+#PostRoute hold timing report
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -postRoute -hold -pathReports -slackReports -numPaths 50 -prefix AGC_postRoute -outDir ../Reports/Layout/timingReports/PostRoutehold
+#DRC errors and timing violations fixing
+setOptMode -fixCap false -fixTran false -fixFanoutLoad false
+optDesign -postRoute -incr
+optDesign -postRoute -hold -incr
+setOptMode -fixCap false -fixTran false -fixFanoutLoad false
+optDesign -postRoute -incr
+optDesign -postRoute -hold -incr
+setOptMode -fixCap false -fixTran false -fixFanoutLoad false
+optDesign -postRoute -hold -incr
+#PostRoute setup timing report after DRC errors fixing
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -postRoute -pathReports -drvReports -slackReports -numPaths 50 -prefix AGC_postRoute -outDir ../Reports/Layout/timingReports/PostRoutesetupDRCfixed
+#PostRoute hold timing report after DRC errors and timing violations fixing
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -postRoute -hold -pathReports -slackReports -numPaths 50 -prefix AGC_postRoute -outDir ../Reports/Layout/timingReports/PostRouteholdDRCfixed
+#Placing fillers
+getFillerMode -quiet
+addFiller -cell FEED7HD FEED5HD FEED3HD FEED2HD FEED25HD FEED1HD FEED15HD FEED10HD DECAP7HD DECAP5HD DECAP3HD DECAP25HD DECAP15HD DECAP10HD -prefix FILLER
+#Verifications
+setVerifyGeometryMode -area { 0 0 0 0 } -minWidth true -minSpacing true -minArea true -sameNet true -short true -overlap true -offRGrid false -offMGrid true -mergedMGridCheck true -minHole true -implantCheck true -minimumCut true -minStep true -viaEnclosure true -antenna false -insuffMetalOverlap true -pinInBlkg false -diffCellViol true -sameCellViol false -padFillerCellsOverlap true -routingBlkgPinOverlap true -routingCellBlkgOverlap true -regRoutingOnly false -stackedViasOnRegNet false -wireExt true -useNonDefaultSpacing false -maxWidth true -maxNonPrefLength -1 -error 1000 -report ../Reports/Layout/Verify/AGC.geom.rpt
+verifyGeometry
+setVerifyGeometryMode -area { 0 0 0 0 }
+verify_drc -report ../Reports/Layout/Verify/AGC.drc.rpt -limit 1000
+verifyConnectivity -type all -report ../Reports/Layout/Verify/AGC.conn.rpt -error 1000 -warning 50
+#SignOff
+setExtractRCMode -engine postRoute -effortLevel signoff
+extractRC
+#SignOff setup timing report
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -signoff -pathReports -drvReports -slackReports -numPaths 50 -prefix AGC_signOff -outDir ../Reports/Layout/timingReports/SignOffsetup
+#SignOff hold timing report
+redirect -quiet {set honorDomain [getAnalysisMode -honorClockDomains]} > /dev/null
+timeDesign -signoff -hold -pathReports -slackReports -numPaths 50 -prefix AGC_signOff -outDir ../Reports/Layout/timingReports/SignOffhold
+all_hold_analysis_views 
+all_setup_analysis_views 
+#Generating output files
+write_sdf  ../Outputs/Layout/AGC.sdf
+saveNetlist ../Outputs/Layout/AGC_TOP_netlist.v
+saveNetlist ../Outputs/Layout/AGC_netlist_Phys.v -includePhysicalCell {FEED7HD FEED5HD FEED3HD FEED2HD FEED25HD FEED1HD FEED15HD FEED10HD DECAP7HD DECAP5HD DECAP3HD DECAP25HD DECAP15HD DECAP10HD}
+global dbgLefDefOutVersion
+set dbgLefDefOutVersion 5.8
+defOut -floorplan -netlist -routing ../Outputs/Layout/AGC.def
+set dbgLefDefOutVersion 5.8
+saveDesign ../Outputs/Layout/AGC.enc
